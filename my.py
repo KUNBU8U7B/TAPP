7080cb7d3225f141387d7b589d008e4b309167cb
KUNBU8U7B <gedeagusdharmaputra6@gmail.com>
2026-08-07 19:54:23 +0800

    k

---
+++ my.py
@@ -0,0 +1,18 @@
+from textual.app import App , ComposeResult
+from textual.weights import Header , Footer , Button , Static
+
+class Launcher(App):
+    CSS_PATH = "style.tcss" # import file css
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Static(
+            "[bold cyan]⚡ CYBER LAUNCHER TUI ⚡[/bold cyan]\n\n"
+            "Tampilan ini diatur 100% dari file [yellow]style.tcss[/yellow]!",
+            id="kotak-launcher"
+        )
+        yield Button("Buka APP")
+        yield Footer()
+
+if __name__ == "__main__" :
+    app = Launcher()
+    app.run()
